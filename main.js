bb86aa70cd3a37caf1891f6bc3c357139512a9b5
topazus <topazus@outlook.com>
2022-06-25 23:10:09 +0800

    commit 1

---
+++ main.js
@@ -0,0 +1,4 @@
+import assert from "node:assert/strict";
+
+assert.equal("1.2" |> Number(%), 1.2);
+console.log(1 |> % + 1);
---
+++ output.js
@@ -0,0 +1,3 @@
+import assert from "node:assert/strict";
+assert.equal(Number("1.2"), 1.2);
+console.log(1 + 1);
